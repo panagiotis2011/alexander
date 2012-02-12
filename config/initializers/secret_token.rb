7c980d38059bf9745f323a7c81abe97567e9e5c5
panagiotis2011 <sch@sch.gr>
2012-02-12 17:51:40 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alexander::Application.config.secret_token = 'f1fd8435bae9c2500b14ac7018376285f00708de14f02371777aa53b5bf70a4e2a3ffda69a198a0ada9a9ec64bc8d8d7aa135cd8b278c531880e65801f05f522'
